f59ce1c2670dff5c9f6a92fa5f4a64e9f1438b2e
SatyajitMohanty57 <mohantysatyajit57@gmail.com>
2025-03-15 11:56:29 -0400

    basic auth

--- src/main/java/org/example/springsecuritydemo/controller/HelloController.java
+++ src/main/java/org/example/springsecuritydemo/controller/HelloController.java
@@ -1,2 +1,17 @@
-package org.example.springsecuritydemo.controller;public class HelloController {
+package org.example.springsecuritydemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/")
+    public String getUsers(HttpServletRequest request) {
+        return "Satyajit " + request.getSession().getId();
+    }
 }
